0178766c8f856436b237f4040016bf90bee86e44
YASH <30521446+yashkej1@users.noreply.github.com>
2019-11-30 01:11:03 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,5 +1,4 @@
-SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\gcloud
-SET BUILD_NUMBER=2
+SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
 SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
 cd E:\Downloads\Jenkins-pymicro\Source
 kubectl set image deployment/g-pymicrocluster g-pymicrocluster=%IMAGE_NAME%
