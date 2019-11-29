f06b922065e5077db4ba0603a28a64ea2e2d6a8d
yashkej1 <yashkejriwal23@gmail.com>
2019-11-29 18:55:56 +0530

    New Changes

--- Source/docker-image-build.bat
+++ Source/docker-image-build.bat
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\gcloud
 SET IMAGE_NAME=gcr.io/new-project-258611/gpymicro-image@sha256:7f2f5b8a564502fabda0fa9ad95214370cf4a01870a7cd9f8fdad5ea93488b71:%BUILD_NUMBER%
-cd E:\Downloads\PyMicroservices-master\Source
+cd E:\Downloads\Jenkins-pymicro\Source
 gcloud builds submit --tag %IMAGE_NAME% .
--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,4 +1,5 @@
 SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin\gcloud
+SET BUILD_NUMBER=2
 SET IMAGE_NAME=gcr.io/handle-books/gpymicro-image:%BUILD_NUMBER%
-cd E:\Downloads\PyMicroservices-master\Source
+cd E:\Downloads\Jenkins-pymicro\Source
 kubectl set image deployment/g-pymicrocluster g-pymicrocluster=%IMAGE_NAME%
