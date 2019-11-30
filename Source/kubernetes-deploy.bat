ddf20642b1f29b4614fa92cfd0fbeee7ecc3ede6
YASH <30521446+yashkej1@users.noreply.github.com>
2019-11-30 16:04:38 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,5 +1,4 @@
 SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk
 SET IMAGE_NAME=gcr.io/new-project-258611/gpymicro-image:%BUILD_NUMBER%
 cd E:\Downloads\Jenkins-pymicro\Source
-kubectl get pods
-kubectl set image deployment/g-pymicrocluster g-pymicrocluster=%IMAGE_NAME%
+start cmd.exe /k "kubectl set image deployment/g-pymicrocluster g-pymicrocluster=%IMAGE_NAME%"
