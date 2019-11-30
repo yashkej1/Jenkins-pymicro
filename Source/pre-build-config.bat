27e599a40967e5a8127e312076a5e9cccff4d2ee
YASH <30521446+yashkej1@users.noreply.github.com>
2019-11-30 13:46:24 +0530

    Update pre-build-config.bat

--- Source/pre-build-config.bat
+++ Source/pre-build-config.bat
@@ -1,3 +1,3 @@
 SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
 git pull origin master
-gcloud auth activate-service-account jenkins12@new-project-258611.iam.gserviceaccount.com --key-file=E:\Downloads\Keys\new-project-258611-a6ccd9ee5d87.json --project=new-project-258611
+gcloud auth activate-service-account jenkins12@new-project-258611.iam.gserviceaccount.com --key-file=E:\Downloads\Keys\new-project-258611-dd737768492a.json --project=new-project-258611
