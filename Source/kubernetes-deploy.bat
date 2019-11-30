d07518f6d98104d73119fe9301f76821bde7a884
YASH <30521446+yashkej1@users.noreply.github.com>
2019-11-30 14:24:39 +0530

    Update kubernetes-deploy.bat

--- Source/kubernetes-deploy.bat
+++ Source/kubernetes-deploy.bat
@@ -1,4 +1,4 @@
-SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin
+SET PATH=%PATH%;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk\bin;C:\Users\ASUS\AppData\Local\Google\Cloud SDK\google-cloud-sdk
 SET IMAGE_NAME=gcr.io/new-project-258611/gpymicro-image:%BUILD_NUMBER%
 cd E:\Downloads\Jenkins-pymicro\Source
 kubectl get pods
